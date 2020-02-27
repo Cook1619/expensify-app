41dfad99d66a58fb819b5d299a344b2a9d30e230
Cook1619 <cook6348@gmail.com>
2020-02-26 19:53:51 -0600

    Setting up switch in the router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
@@ -11,14 +11,18 @@ const AddExpensePage = () => <div>This is my expense page</div>;
 const EditExpensePage = () => <div>This is my edit page</div>;
 
 const HelpPage = () => <div>This is my help page</div>;
+
+const NotFoundPage = () => <div>404!!!</div>;
+
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true} />
       <Route path="/create" component={AddExpensePage} />
       <Route path="/edit" component={EditExpensePage} />
       <Route path="/help" component={HelpPage} />
-    </div>
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 );
 
