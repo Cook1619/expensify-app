0d88d9808a02243d242060c5bcb2713dc6925f70
Cook1619 <cook6348@gmail.com>
2020-02-27 20:11:47 -0600

    Added each component to their own files

---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const EditExpensePage = () => <div>This is my edit page</div>;
+
+
+export default EditExpensePage;
---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const ExpenseDashboardPage = () => <div>Dashboard!!</div>;
+
+export default ExpenseDashboardPage;
