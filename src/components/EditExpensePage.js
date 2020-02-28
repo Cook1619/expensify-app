c158c8df54e0d8ae8d5e6034cde90f1b83fac549
Cook1619 <cook6348@gmail.com>
2020-02-28 15:40:37 -0600

    Adding redux to playground app

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const EditExpensePage = () => <div>This is my edit page</div>;
-
+const EditExpensePage = (props) => {
+    console.log('PROPSS', props)
+    return (
+        <div>Editing page with id</div>
+    )
+}
 
 export default EditExpensePage;
