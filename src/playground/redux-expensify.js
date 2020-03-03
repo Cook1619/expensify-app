deae6fa12ae52e5672094214c60af2000c0b8405
Cook1619 <cook6348@gmail.com>
2020-03-03 12:34:32 -0600

    Added 2 reducers and used combined reducers with create store

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,18 +1,56 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers } from "redux";
 
-const demoState = {
-    expenses: [{
-        id: 'fdgdfd',
-        description: 'Testing',
-        notes: 'This is a bigger test, hooray',
-        amount: 50000,
-        createdAt: 0
-    }],
-    filters: {
-        text: 'rent',
-        sortBy: 'amount', //date or amount
-        startDate: undefined,
-        endDate: undefined
-    }
+// Expenses Reducer
+
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
 };
 
+//Filter Reducer
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+}
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+//Store creation
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+);
+
+console.log(store.getState());
+
+const demoState = {
+  expenses: [
+    {
+      id: "fdgdfd",
+      description: "Testing",
+      notes: "This is a bigger test, hooray",
+      amount: 50000,
+      createdAt: 0
+    }
+  ],
+  filters: {
+    text: "rent",
+    sortBy: "amount", //date or amount
+    startDate: undefined,
+    endDate: undefined
+  }
+};
