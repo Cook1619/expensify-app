b6f61f53643d935fea10890a04e8bd345ac4775e
Cook1619 <cook6348@gmail.com>
2020-03-02 19:16:03 -0600

    Setting up reducers, and dummy daat for app

--- src/playground/redux.js
+++ src/playground/redux.js
@@ -10,7 +10,7 @@ const decrementCount = ({ decrementBy = 1 } = {}) => ({
   decrementBy
 })
 
-const setCount = ({ count = 1 } = {}) => ({
+const setCount = ({ count }) => ({
   type: 'SET',
   count
 })
@@ -19,7 +19,11 @@ const resetCount = () => ({
   type: 'RESET',
 })
 
-const store = createStore((state = { count: 0 }, action) => {
+//Reducers
+// Reducers are pure functions
+// Never change state or action
+
+const countReducer = (state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
       return {
@@ -40,7 +44,10 @@ const store = createStore((state = { count: 0 }, action) => {
     default:
       return state;
   }
-});
+}
+
+const store = createStore(countReducer);
+
 //everytime there is a change to the store this gets called or when state changes
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState()); 
