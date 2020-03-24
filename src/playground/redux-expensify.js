4c6cc4bd9b0a7bdac681c5992c5771711ab1d302
Cook1619 <cook6348@gmail.com>
2020-03-23 20:12:40 -0500

    Adding redux to expense tracker

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,14 +1,16 @@
-import { createStore, combineReducers } from "redux";
-import uuid from "uuid";
+import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
-// ADD-EXPENSE
-const addExpense = ({
-  description = "",
-  note = "",
-  amount = 0,
-  createdAt = 0
-} = {}) => ({
-  type: "ADD_EXPENSE",
+// ADD_EXPENSE
+const addExpense = (
+  {
+    description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
   expense: {
     id: uuid(),
     description,
@@ -17,31 +19,43 @@ const addExpense = ({
     createdAt
   }
 });
+
 // REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
+
 // EDIT_EXPENSE
 // SET_TEXT_FILTER
-// SORT_BY DATE
+// SORT_BY_DATE
 // SORT_BY_AMOUNT
 // SET_START_DATE
 // SET_END_DATE
-// Expenes Reducer
+
+// Expenses Reducer
 
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
-    case "ADD EXPENSE":
-      return [...state, action.expense];
+    case 'ADD_EXPENSE':
+      return [
+        ...state,
+        action.expense
+      ];
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => id !== action.id);
     default:
       return state;
   }
 };
 
-// Filters reducer
+// Filters Reducer
 
 const filtersReducerDefaultState = {
-  text: "",
-  sortBy: "date",
+  text: '',
+  sortBy: 'date',
   startDate: undefined,
   endDate: undefined
 };
@@ -53,7 +67,7 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
   }
 };
 
-//Store Creation
+// Store creation
 
 const store = createStore(
   combineReducers({
@@ -66,22 +80,24 @@ store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch(addExpense({ description: "Rent", amount: 100 }));
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 const demoState = {
-  expenses: [
-    {
-      id: "dfdsfsdsd",
-      description: "Rent",
-      notes: "This is for the final payment for rent",
-      amount: 54500,
-      createdAt: 0
-    }
-  ],
+  expenses: [{
+    id: 'poijasdfhwer',
+    description: 'January Rent',
+    note: 'This was the final payment for that address',
+    amount: 54500,
+    createdAt: 0
+  }],
   filters: {
-    text: "rent",
-    sortBy: "amount", //date or amount
+    text: 'rent',
+    sortBy: 'amount', // date or amount
     startDate: undefined,
     endDate: undefined
   }
 };
+
