8edd1ba3d1f4505026ac52a1b552809af5b98c3d
Cook1619 <cook6348@gmail.com>
2020-03-24 19:53:00 -0500

    Setting up input with filtering search

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,6 +1,12 @@
-import React from 'react'
-import ExpenseList from './ExpenseList'
+import React from "react";
+import ExpenseList from "./ExpenseList";
+import ExpenseListFilters from "./ExpenseListFilters";
 
-const ExpenseDashboardPage = () => <div><ExpenseList /></div>;
+const ExpenseDashboardPage = () => (
+  <div>
+    <ExpenseListFilters />
+    <ExpenseList />
+  </div>
+);
 
 export default ExpenseDashboardPage;
